84c9b6d9d2bd04f5c5b4dd0921593afae4b5b0ce
Artamta <artamta47@gmail.com>
2025-10-31 04:59:29 +0530

    Update

--- slurm.py
+++ slurm.py
@@ -1,2 +1,6 @@
+"""
 srun --partition=gpu --gres=gpu:1 --cpus-per-task=32 --mem=64 --pty bash
 salloc --partition=iiser --gres=gpu:1 --cpus-per-task=32 --mem=256G
+salloc --partition=iiser --gres=gpu:1 --cpus-per-task=32 --mem=256G --job-name=Fuxi_run
+
+"""
