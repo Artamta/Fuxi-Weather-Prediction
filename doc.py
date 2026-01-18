fdc6f6e8ea8c2f2aa83d04ccd4e7dc0cbfa29624
Artamta <artamta47@gmail.com>
2026-01-18 16:46:41 +0530

    brainstorming

---
+++ doc.py
@@ -0,0 +1,14 @@
+#model v1 -> 70 vars
+#model v2--> 20 vars
+
+#20 variable 
+#5_surface 
+#13_P_Levels
+
+#So I am getting error --> to calculating acc,rmse
+#using_normalistion and denormlistion 
+
+# Retrain model on smaller dataset : to test
+# check to evolute model
+# how good it is prediction ,
+# swin undet and undet swin (idk)
