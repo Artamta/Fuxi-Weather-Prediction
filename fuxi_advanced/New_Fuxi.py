8056117ffe496f7a58888cf357f1204e69420e98
Artamta <artamta47@gmail.com>
2026-02-08 20:57:08 +0530

    build_advanced_fuxi

---
+++ fuxi_advanced/New_Fuxi.py
@@ -0,0 +1,11 @@
+'''
+Building a new fuxi model "not swin bottelnect but using a utransformeer"
+'''
+import train_dist
+import csv
+
+def work_founction(fun):
+    user_name="raj.ayush"
+    cluster_pass = " "
+    Dir = fuxi(adv)
+    return Dir 
